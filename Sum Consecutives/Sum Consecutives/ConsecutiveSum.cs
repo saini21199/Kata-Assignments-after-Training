002c9766cb5e4f90bf4a1e364c1eb903d6965c24
saini21199 <sachinsainiandromeda007@gmail.com>
2021-05-11 00:44:34 +0530

    Completed all problem, Consecutive sum done

---
+++ Sum Consecutives/Sum Consecutives/ConsecutiveSum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sum_Consecutives
+{
+    public class ConsecutiveSum
+    {
+        public List<int> s { get; set; }
+        public ConsecutiveSum(List<int> list) {
+            this.s = list;
+        }
+        public void findSumConsecutives()
+        {
+            int count = 1;
+            for (int i = 0; i < s.Count - 1; i++)
+            {
+                count = 1;
+                if (s[i] == s[i + 1])
+                {
+                    int index = i;
+                    int temp = s[i];
+                    while (s[i] == s[i + 1])
+                    {
+                        s.RemoveAt(i + 1);
+                        count++;
+                    }
+                    s.RemoveAt(index);
+                    s.Insert(index, count * temp);
+                }
+            }
+           
+            Console.WriteLine("List after finding Sum of consecutive elements");
+            s.ForEach(i => Console.WriteLine(i));
+        }
+    }
+}
---
+++ Sum Consecutives/Sum Consecutives/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sum_Consecutives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many elements do you want to add in list");
+            int listSize = Convert.ToInt32(Console.ReadLine());
+
+            List<int> lst = new List<int>();
+
+            Console.WriteLine("Enter elements:");
+            for (int i = 0; i < listSize; i++)
+            {
+                int x=Convert.ToInt32(Console.ReadLine());
+                lst.Add(x);
+            }
+            Console.Clear();
+            ConsecutiveSum operation = new ConsecutiveSum(lst);
+            operation.findSumConsecutives();
+        }
+    }
+}
